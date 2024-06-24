9c9f22874de799dade3a8a4ac140a1fba3978643
Antonio Leskur <antonioleskur93@gmail.com>
2024-06-24 20:54:23 +0200

    little work

--- app.py
+++ app.py
@@ -3,27 +3,24 @@ from datetime import date
 
 app = Flask(__name__)
 
+today = str(date.today())
 
 @app.route("/")
 def home():
-    today = str(date.today())
     return render_template("index.html", time_stamp=today)
 
 
 @app.route("/articles")
 def articles():
-    today = str(date.today())
     return render_template("articles.html", time_stamp=today)
 
 
 @app.route("/contact")
 def contact():
-    today = str(date.today())
     return render_template("contact.html", time_stamp=today)
 
 @app.route("/documentaries")
 def documentaries():
-    today = str(date.today())
     return render_template("documentaries.html", time_stamp=today)
 
 
